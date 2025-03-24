d0566c6a4d438204581827ce181bc227d61b0fe1
Matheus Honorato <matheuswebmw@gmail.com>
2025-03-23 21:48:20 -0300

    feat: adicionado insert no banco de dados

--- src/classes/postgresql.py
+++ src/classes/postgresql.py
@@ -1,5 +1,18 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 import pandas as pd
+import numpy as np 
+import psycopg2.extras as extras 
+
+
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST") 
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 
 class Postgresql:
@@ -22,10 +35,26 @@ class Postgresql:
             self.cursor.execute(query)
             results = self.cursor.fetchall()
             df = pd.DataFrame(results, columns=[desc[0] for desc in self.cursor.description])
-            print(df)
-            
-            # return df
+            return df
+
         except (Exception, psycopg2.Error) as error:
             print("Ocorreu um erro na conexão do SQL:", error)
         finally:
             self.close_connection()
+
+    def insert(self, df, name_table):
+        tuples = [tuple(x) for x in df.to_numpy()] 
+        colums = ','.join(list(df.columns)) 
+        try:
+            self.open_connection()
+            query = "INSERT INTO %s(%s) VALUES %%s" % (name_table, colums) 
+            extras.execute_values(self.cursor, query, tuples) 
+            self.conn.commit()
+            print("Dados inserido com sucesso...")
+        except (Exception, psycopg2.Error) as error:
+            self.conn.rollback()
+            print("Ocorreu um erro na conexão do SQL:", error)
+        finally:
+            self.close_connection()
+
+
