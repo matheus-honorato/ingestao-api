725b56ac624dc1a77d1063a68f42d85314db94fc
Matheus Honorato <matheuswebmw@gmail.com>
2025-03-26 22:05:03 -0300

    fix(docs): mudança no método e adicionado docstring

--- src/classes/api.py
+++ src/classes/api.py
@@ -1,12 +1,16 @@
 import requests
-from dotenv import load_dotenv
 import os
 import logging
 
-load_dotenv()  # Identifica as variaveis de ambiente criada em .env e conseguimos utilizar no código
-
 
 class Api:
+    """
+    Classe que interage com a API do TMDB para acessar dados de filmes e séries.
+
+    Esta classe fornece métodos para fazer requisições à API do TMDB e retornar
+    informações sobre filmes populares ou séries populares. 
+
+    """
     base_url = "https://api.themoviedb.org/3"
 
     def __init__(self):
@@ -18,7 +22,18 @@ class Api:
             "language": "pt-BR"
         }
 
-    def request(self, endpoint, extra_params=None):
+    def request(self, endpoint, extra_params=None) -> dict:
+        """
+        Faz uma requisição GET para o endpoint especificado.
+
+        Esse método constrói a URL com base na URL base e no endpoint, e envia uma requisição HTTP GET.
+        Permite adicionar parâmetros extras à requisição conforme a necessidade.
+
+        :param endpoint: O caminho do endpoint a ser acessado (str).
+        :param extra_params: Parâmetros adicionais para a requisição (dict, opcional).
+        :return: Resposta da requisição em formato JSON (dict) ou um dicionário de erro (dict) em caso de falha.
+        :raises requests.exceptions.RequestException: Se ocorrer um erro durante a requisição.
+        """
         url = f"{self.base_url}{endpoint}"
         params = self.params.copy()
         if extra_params:
@@ -30,15 +45,23 @@ class Api:
             return response.json()
         except requests.exceptions.RequestException as e:
             logging.error(f"Erro na requisição {url}: {e}")
-            return None
+            return {"error": str(e)}
 
-    def get_filmes_populares_tmbd(self):
+    def get_filmes_populares_tmbd(self, time_window="day") -> dict:
+        """
+        Retorna os filmes mais populares do TMDB com base nas tendências diárias.
+
+        :return: Um dicionário contendo os filmes populares.
+        """
         time_window = "day"
         endpoint = f"/trending/movie/{time_window}" 
         return self.request(endpoint)
 
-    def get_series_populares_tmbd(self):
-        time_window = "day" 
+    def get_series_populares_tmbd(self, time_window="day") -> dict:
+        """
+        Retorna as séries mais populares do TMDB com base nas tendências diárias.
+
+        :return: Um dicionário contendo os filmes populares.
+        """
         endpoint = f"/trending/tv/{time_window}"
         return self.request(endpoint)
-  
